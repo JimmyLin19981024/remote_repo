9cc94cc2aed6d83171e1fe5d05a78c51e1151546
Jimmy Lin <jimmylin19981024@gmail.com>
2019-02-28 15:25:27 +0800

    test the change

---
+++ test.py
@@ -0,0 +1,44 @@
+import dis
+import time
+import math
+
+
+def timeit(func):
+    def timed(*arg, **kwarg):
+        start = time.time()
+        result = func(*arg, **kwarg)
+        end = time.time()
+        print(f'function "{func.__name__}" takes {end - start} to run.')
+        return result
+    return timed
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+head = Node(4)
+nodeB = Node(3)
+nodeC = Node(2)
+nodeD = Node(1)
+
+head.next = nodeB
+nodeB.next = nodeC
+nodeC.next = nodeD
+
+
+def countNodes(head):
+    count = 1
+    while head.next != None:
+        count += 1
+        head = head.next
+    return count
+
+
+print(countNodes(head))
+
+
+def test2():
+    print("testing!")
