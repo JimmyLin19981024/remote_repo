ad79343b5c2aaaf03488f159b51c2c65f6879996
Jimmy Lin <jimmylin19981024@gmail.com>
2019-03-06 19:57:17 +0800

    linked list

--- DataStructureAndAlgorithm/linkedList.cpp
+++ DataStructureAndAlgorithm/linkedList.cpp
@@ -13,6 +13,7 @@ class linkedList
 public:
     linkedList();
     ~linkedList();
+    bool isEmpty() { return head==NULL; };
     void diplayList();
     Node* addNewNode(int index, double value);
     int findNode(double value);
@@ -22,24 +23,80 @@ private:
     int length;
 };
 
-linkedList::linkedList()
-{
+linkedList::linkedList(){
     this->head = NULL;
     this->length = 0;
 }
 
-linkedList::~linkedList()
-{
+linkedList::~linkedList(){
     Node* next = head;
     Node* cur = NULL;
-    while (next!=NULL)
-    {
+    while(next!=NULL){
         cur = next;
-        next = next.next;
+        next = next->next;
         delete cur;
+    }
+    cout << "linkedList deleted!" << endl;
+}
+
+void linkedList::diplayList(){
+    Node* head = head;
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
     }
 }
 
+Node* linkedList::addNewNode(int index, double value){
+    if(index<0) return NULL;
+    
+    int currIndex = 0;
+    Node* currNode = head;
+    while(currNode and currIndex!=index){
+        currNode = currNode->next;
+        currIndex++;
+    }
+    
+    if(index>0 and currNode==NULL) return NULL;
+    
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = currNode->next;
+    currNode->next = newNode;
+    return newNode;
+}
+
+int linkedList::findNode(double value){
+    Node* currNode = head;
+    int currIndex = 0;
+    while(currNode and currNode->data!=value) {
+        currNode = currNode->next;
+        currIndex++;
+    }
+    return currIndex;
+}
+
+int linkedList::deleteNode(double value){
+    Node* prevNode = NULL;
+    Node* currNode = head;
+    int currIndex = 0;
+    while(currNode and currNode->data!=value){
+        prevNode = currNode;
+        currNode = currNode->next;
+        currIndex++;
+    }
+    if(currNode){
+        if(prevNode){
+            prevNode->next = currNode->next;
+            delete currNode;
+        }
+    }
+    return currIndex;
+}
+
 int main(int argc, const char * argv[]) {
+    linkedList* list = new linkedList;
+    list->addNewNode(2,5.0);
+//    list->diplayList();
     return 0;
 }
